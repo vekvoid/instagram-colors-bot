e558fb8b1af3a84a103dcac8bfb3ac6bf9071d84
vekvoid <vektrm@gmail.com>
2017-10-03 14:14:14 +0200

    Module to generate random colors

---
+++ app/random-color.js
@@ -0,0 +1,22 @@
+const randomColor = () => {
+  const newColor = {};
+  let newHexColor = '#FFFFFF';
+
+  newColor.r = getRandomInt(0, 255);
+  newColor.g = getRandomInt(0, 255);
+  newColor.b = getRandomInt(0, 255);
+
+  newHexColor = rgbToHex(newColor.r, newColor.g, newColor.b);
+
+  return newHexColor;
+};
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function rgbToHex(r, g, b) {
+  return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+module.exports = randomColor;
